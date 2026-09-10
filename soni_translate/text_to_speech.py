50e7a24ae42f9cd25c4f37e01480257653c54492
CG227876 <cg227876@gmail.com>
2026-09-10 13:04:25 +0800

    Update text_to_speech.py

--- soni_translate/text_to_speech.py
+++ soni_translate/text_to_speech.py
@@ -1161,8 +1161,8 @@ def accelerate_segments(
 
         if acc_percentage > max_accelerate_audio:
             acc_percentage = max_accelerate_audio
-        elif acc_percentage < 1.0:
-            acc_percentage = 1.0
+        elif acc_percentage < 1.1:
+            acc_percentage = 1.1
 
         # Round
         acc_percentage = round(acc_percentage + 0.0, 1)
